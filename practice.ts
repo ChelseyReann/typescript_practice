cb2ab7ff89d23363e294088baee96e30a71d6e2a
ChelseyReann <chelseyreann@gmail.com>
2023-08-02 13:53:14 -0400

    adds new challenge

---
+++ practice.ts
@@ -0,0 +1,8 @@
+/* 
+    CHALLENGE
+        make a function called addNums that takes in 2 numbers, 
+        adds them together and returns the result as a string.
+        For example, if I did: 
+            console.log(addNums(1, 2))
+         I would get 3 as a string in the console.
+*/
